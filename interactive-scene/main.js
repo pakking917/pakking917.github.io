73e305b8b0ed10de7e21e47b4fcaffcee39615b8
Lee7009201 <7009201@spsd.sk.ca>
2026-02-27 11:03:35 -0600

    finally added heart movement and empty variables for levels

--- interactive-scene/main.js
+++ interactive-scene/main.js
@@ -5,6 +5,7 @@
 // Extra for Experts:
 // - Resizing window
 // - Local storage for death count
+// - Editted html and extra files to better contain level info
 
 
 // Game env. setup
@@ -12,10 +13,14 @@ let gameState = "intro";
 let box = {};
 let spiteScale;
 let baseSize;
+const LEVELS = [level1, level2, level3, level4, level5];
+let iframes = 0;
+
 
 // Player and sans setup
 let sansX, sansY;
 let heartX, heartY;
+let heartVX = 0, heartVY = 0;
 let currentLevel = 1;
 const playerMaxHP = 92;
 let playerHP = playerMaxHP;
@@ -31,13 +36,11 @@ let dialogueDone  = false;
 let dialoguePhase = "pre"; // pre for pre battle and post for post battle
 
 function setup() {
-  baseSize = Math.min(windowWidth / 4, windowWidth / 3)
+  calcSizes();
   createCanvas(baseSize * 4, baseSize * 3);
-  sansX = width / 2;
-  sansY = height / 4;
-  heartX = width / 2;
-  heartY = height * 7 / 10;
-  spiteScale = baseSize / 100 / 2.5
+  spiteScale = baseSize / 100 / 2.5;
+  resetPlayer();
+  box = makeBattleBox();
 }
 
 function draw() {
@@ -51,7 +54,7 @@ function draw() {
 
 
 function calcSizes() {
-  baseSize   = Math.floor(Math.min(windowWidth / 4, windowHeight / 3));
+  baseSize   = Math.max(Math.floor(Math.min(windowWidth / 4, windowHeight / 3)), 100);
   heartSpeed = baseSize * 0.03;
 }
 
@@ -60,13 +63,13 @@ function windowResized() {
   let heartPosX = heartX / width
   let heartPosY = heartY / height
   resizeCanvas(baseSize * 4, baseSize * 3);
-  spiteScale = baseSize / 100 / 2.2;
+  spiteScale = baseSize / 100 / 2.5;
   sansX = width / 2;
   sansY = height / 4;
   box = makeBattleBox();
   if (gameState !== 'title') {
-    heartX = constrain(heartPosX * width, box.x + 8, box.x + box.w - 8);
-    heartY = constrain(heartPosY * height, box.y + 8, box.y + box.h - 8);
+    heartX = constrain(heartPosX * width + heartVX, box.x + 8, box.x + box.w - 8);
+    heartY = constrain(heartPosY * height + heartVY, box.y + 16, box.y + box.h);
   }
 }
 
@@ -77,6 +80,19 @@ function makeBattleBox() {
     w: width  * 0.8,
     h: height * 0.38
   };
+}
+
+function resetPlayer() {
+  spiteScale = baseSize / 100 / 2.5;
+  sansX = width  / 2;
+  sansY = height / 4;
+  box = makeBattleBox();
+  heartX = box.x + box.w / 2;
+  heartY = box.y + box.h / 2;
+  iframes = 0;
+  activeAttacks = [];
+  attackQueue   = [];
+
 }
 
 function getDeaths() { 
@@ -100,13 +116,36 @@ function drawIntro() {
   rect(width / 10, height / 2 , width * 4 / 5, height * 4 / 10)
   
   
-  console.log(width, height, sansX, sansY, heartX, heartY)
+  console.log(width, height, sansX, sansY, heartX, heartY) //test
   
   drawHeart(heartX, heartY, spiteScale);
+  updateHeart();
 }
 
 
+function updateHeart() {
+  heartVX = 0; heartVY = 0;
+  if (keyIsDown(LEFT_ARROW)  || keyIsDown(65)) {
+    heartVX = -heartSpeed;
+  }
+  if (keyIsDown(RIGHT_ARROW) || keyIsDown(68)) {
+    heartVX =  heartSpeed;
+  }
+  if (keyIsDown(UP_ARROW)    || keyIsDown(87)) {
+    heartVY = -heartSpeed;
+  }
+  if (keyIsDown(DOWN_ARROW)  || keyIsDown(83)) {
+    heartVY =  heartSpeed;
+  }
 
+  // Normalise diagonal speed
+  if (heartVX !== 0 && heartVY !== 0) {
+    heartVX *= 0.707; heartVY *= 0.707; 
+  }
+
+  heartX = constrain(heartX + heartVX, box.x + 8, box.x + box.w - 8);
+  heartY = constrain(heartY + heartVY, box.y + 16, box.y + box.h);
+}
 
 
 

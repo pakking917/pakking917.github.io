4b46efa05678d29f482dbd680f95ef600fa745f7
pakking917 <80308233+pakking917@users.noreply.github.com>
2026-03-03 11:02:31 -0600

    demo 4

---
+++ 04-ball-array/sketch.js
@@ -0,0 +1,34 @@
+// Ball Array
+
+let ballArray = [];
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+  for (let ball of ballArray) {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+
+    circle(ball.x, ball.y, ball.radius * 2);
+}
+
+function mousePressed() {
+  spawnBall();
+}
+
+function spawnBall() {
+  let someBall = {
+    x: random(width),
+    y: random(height),
+    dx: random(-5, 5),
+    dy: random(-5, 5),
+    radius: random(10, 30)
+  }
+
+  ballArray.push(someBall);
+}
--- index.md
+++ index.md
@@ -4,6 +4,7 @@
 - [First Day Demo](01-first-demo)
 - [Scope](02-scope)
 - [Les images](03-images)
+- [Les balls](04-ball-array)
 
 ## Assignments
 - [Interactive Scene](interactive-scene)

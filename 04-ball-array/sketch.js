af67b53cf680c92b9a6ebc76d2ae4c1d4ea6d735
Lee7009201 <7009201@spsd.sk.ca>
2026-03-04 11:03:12 -0600

    demo 4 continued

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -14,20 +14,31 @@ function draw() {
     ball.x += ball.dx;
     ball.y += ball.dy;
 
+    if (ball.x - ball.radius < 0)      {ball.x = width + ball.radius;}
+    if (ball.x + ball.radius > width)  {ball.x = width - ball.radius;}
+    if (ball.y - ball.radius < 0)      {ball.y = height + ball.radius;}
+    if (ball.y + ball.radius > height) {ball.y = height - ball.radius;}
+    
+
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius * 2);
+  }
 }
 
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
+function spawnBall(_x, _y) {
   let someBall = {
-    x: random(width),
-    y: random(height),
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
-    radius: random(10, 30)
+    radius: random(10, 30),
+    r: random(255),
+    g: random(255),
+    b: random(255)
   }
 
   ballArray.push(someBall);

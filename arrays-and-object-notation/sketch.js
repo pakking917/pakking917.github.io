ac9cc91d562bd8b43f7ce546eb18f2ea5346a9fb
Lee7009201 <7009201@spsd.sk.ca>
2026-03-05 11:03:15 -0600

    Update sketch.js

--- arrays-and-object-notation/sketch.js
+++ arrays-and-object-notation/sketch.js
@@ -1,6 +1,6 @@
-// Project Title
-// Your Name
-// Date
+// Arrays and Object Notation
+// Pak King Lee
+// 2026/3/5
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"

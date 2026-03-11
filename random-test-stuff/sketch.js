12f2f146ab466035723552671e43156895362acc
Lee7009201 <7009201@spsd.sk.ca>
2026-03-11 10:39:13 -0600

    maxSpan test

--- random-test-stuff/sketch.js
+++ random-test-stuff/sketch.js
@@ -9,9 +9,26 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   let bgb;
-  console.log(bgb)
+  console.log(bgb);
+  console.log(maxSpan([1,2,3,4,5,6,7,7]));
 }
 
 function draw() {
   background(0);
 }
+
+function maxSpan(nums) {
+  let maxSkibidi = 0;
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = nums.length - 1; j >= 0; j--) {
+      if (nums[j] === nums[i]) {
+        if (j - i + 1 > maxSkibidi) {
+          maxSkibidi = j - i + 1;        
+        }
+        break;
+      }
+    }
+  }
+  return maxSkibidi;
+}
+

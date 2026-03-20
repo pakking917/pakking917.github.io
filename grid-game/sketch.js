1030dfe9c944c8a03d5cb3e4c9694a09d3883384
Lee7009201 <7009201@spsd.sk.ca>
2026-03-20 11:02:01 -0600

    created level 1 test data

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -1,10 +1,11 @@
-// Project Title
-// Your Name
-// Date
+// 2D Grid Game
+// Pak King Lee
+// 2026/3/20
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+const LEVELS = [level1, level2];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);

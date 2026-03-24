6b6905c563211e9959b994b635bffac6547e5e21
Lee7009201 <7009201@spsd.sk.ca>
2026-03-24 11:03:28 -0600

    finished local data structure and started on nested loop to generate level

--- grid-game/sketch.js
+++ grid-game/sketch.js
@@ -5,7 +5,7 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-const STATE = {MENU:0, HELP: 1, LEVEL_SELECT: 2, PLAY: 3};
+const STATE = {MENU: 0, HELP: 1, LEVEL_SELECT: 2, PLAY: 3};
 let currentState = STATE.MENU;
 
 let unlockedLevels = 1;
@@ -23,6 +23,7 @@ let gridOffsetY = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  loadProgress();
 }
 
 function draw() {
@@ -31,4 +32,42 @@ function draw() {
 
 function loadProgress() {
   let data = localStorage.getItem("skibidiToilet");
+  if (data) {
+    let parsed = JSON.parse(data);
+    unlockedLevels = parsed.unlockedLevels || 1;
+    starLevels = parsed.starLevels || [];
+  }
 }
+
+function loadLevel(levelIndex) {
+  currentState = STATE.PLAY;
+  currentLevel = levelIndex;
+
+  let level = LEVELS[levelIndex - 1];
+  players = [{ x: lvl.playerStartingPosition[0][0], y: lvl.playerStartingPosition[0][1] }, 
+             { x: lvl.playerStartingPosition[1][0], y: lvl.playerStartingPosition[1][1] }];
+
+  let rows = level.map.length;
+  let cols = level.map[0].length;          
+
+  mapData = [];
+  for (let y = 0; y < rows; y++) {
+    let row = [];
+    for (let x = 0; x < cols; x++) {
+      let tile = level.map[y][x];
+
+      if (tile === 2) { // let a floor tile replace a box
+        boxes.push({ x: x, y: y });
+        row.push(1);
+      } 
+      else { 
+        row.push(tile);
+      }
+    }
+
+    mapData.push(row);
+  }
+
+  tileSize = Math.min( (width - 100) / cols);
+}
+

8e6ad9901f0cb252dbed112f67714f4acfa48d4a
pakking917 <80308233+pakking917@users.noreply.github.com>
2026-04-15 03:04:08 -0600

    Added bunch of empty levels to debug

--- grid-game/levels.js
+++ grid-game/levels.js
@@ -26,3 +26,129 @@ let level2 = {
     [4, 4, 4, 4, 4]
 ]
 };
+
+let level3 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level4 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level5 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level6 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level7 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level8 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level9 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level10 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
+
+let level11 = {
+  label: "Level 1",
+  optimalMoves: 3, 
+  playerStartingPosition: [[2, 1], [3, 1]], // [ [x, y], [x, y] ]
+  map: [
+    [4, 4, 4, 4, 4, 4], // 0 = wall, 1 = floor, 2 = box, 3 = goal, 4 = void
+    [4, 4, 1, 1, 4, 4], 
+    [4, 0, 1, 1, 1, 4],
+    [4, 1, 1, 1, 2, 4],
+    [4, 1, 3, 3, 0, 4],
+    [4, 4, 4, 4, 4, 4]
+  ]
+};
